94e99b82a4b58f9745786001bd0c6d562978f802
Angellica Araujo <angellica.c.a@gmail.com>
2017-01-16 01:31:24 -0200

    Updating Gauss Seidel method

--- P05GASEI.py
+++ P05GASEI.py
@@ -1,32 +1,58 @@
-def height(m):
-  return len(m)
+def stopByLines(x,k):
+    if(k==0):
+        return False
 
-def GS(m, x0=None, eps=1e-5, max_iteration=100):
+    maxdiff=0
+    tmpdiff=0
+    EPSILON=0.001
 
-    n  = height(m)
-    x0 = [0] * n if x0 == None else x0
-    x1 = x0[:]
+    for i in range(1,len(x),1):
+        tmpdiff = abs(x[i]) - abs(x[i-1])
+        if(tmpdiff > maxdiff):
+            maxdiff = tmpdiff
 
-    for __ in range(max_iteration):
-        for i in range(n):
-            s = sum(-m[i][j] * x1[j] for j in range(n) if i != j) 
-            x1[i] = (m[i][n] + s) / float(m[i][i])
-        if all(abs(x1[i]-x0[i]) < eps for i in range(n)):
-            return x1 
-        x0 = x1[:]    
-    raise ValueError('Nao converge :(')
-"""
-A = [[3,-1,1,-1], [-1,3,-1,7],[1,-1,3,-7]]
-x = GS(A)
+    return (maxdiff < EPSILON)
+
+def norm(x):
+    s=0;
+    l=len(x)
+
+    for i in range(l):
+        s += x[i]**2
+
+    return math.sqrt(s)
+
+
+def GS(A,b):
+    rows    = len(A[0])
+    lines   = len(A)
+    xs      = [0 for i in range(lines)]
+    k=0
+
+    while not stopByLines(xs,k):
+
+        k = k + 1
+
+        for i in range(lines):
+            summ=0
+            for j in range(rows):
+                if not i==j:
+                    summ = summ - (A[i][j]*xs[j])
+
+            xs[i] = ((b[i] + summ) * (1/float(A[i][i])))
+
+    return xs
+
+
+A = [[1,-1,2], [-1,5,-4],[2,-4,6]]
+b = [0,1,0]
+
+x = GS(A, b)
+
 print("Solution for x is: ", x)
-"""
-
-A = [[4,3.2,0.5,9.2],[2.2,3,-0.3,0.9],[-3.1,-0.2,4,7]]
-x = GS(A)
-print("Solution for x is: ", x)
 
 """
-    x1 = 1.8
-    x2 = 2.2
-    x3 = -2.2
+    x1 = -0.5
+    x2 = 0.5
+    x3 = 0.5
 """
